07f1d9845091b03e5dcad29da0757d8c18113012
karrzm <93216772+karrzm@users.noreply.github.com>
2021-12-09 19:12:02 -0500

    Added Search Engine

--- src/app.js
+++ src/app.js
@@ -44,7 +44,18 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "4f7af84cb966fae648af6c5241bc62a5";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Montreal&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "4f7af84cb966fae648af6c5241bc62a5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Montreal");
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
